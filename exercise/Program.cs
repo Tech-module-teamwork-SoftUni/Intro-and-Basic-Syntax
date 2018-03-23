fff779ec4f688613f9a388bd4217bc8e68c9a1fc
McMeri <34557366+McMeri@users.noreply.github.com>
2018-03-23 13:20:42 +0200

    solution for Prob 1

---
+++ exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntroandBasicSyntax
+{
+
+
+  public class StartUp
+        {
+            public static void Main(string[] args)
+            {
+                var firstNum = int.Parse(Console.ReadLine());
+                var secondNum = int.Parse(Console.ReadLine());
+                var tretoNum = int.Parse(Console.ReadLine());
+                var forthNum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{firstNum:D4} {secondNum:D4} {tretoNum:D4} {forthNum:D4}");
+
+            }
+        }
+        
+    }
+
