34d77962efa0f9b77824c114a39431b9103077f5
McMeri <34557366+McMeri@users.noreply.github.com>
2018-03-23 13:44:16 +0200

    documented the code of Solution of Problem 2

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -20,6 +20,9 @@ namespace IntroandBasicSyntax
         
     }
 
+
+
+
 using System;
 
 namespace RectangleArea
@@ -28,14 +31,25 @@ public class StartUp
 {
 public static void Main(string[] args)
 {
+
+//We receive the width and height of rectangle as floating point numbers.
+
 var width = double.Parse(Console.ReadLine());
 var height = double.Parse(Console.ReadLine());
+
+//Here we sum rectangle's area.
+
 var sum = width * height;
+
+//Finally, we print the result, formatted to the 2nd character after the decimal point.
+
 Console.WriteLine($"{sum:f2}");
 
 }
 }
 }
+
+
 
 using System;
 
